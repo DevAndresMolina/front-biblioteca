ad964779f0dd3a475d905e74e5e45f66de726095
DevAndresMolina <andresmolina_099105@hotmail.com>
2024-07-01 22:53:25 -0500

    Actualización  proyecto

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { Container, Row, Col, Nav, Link} from "react-bootstrap";
-
+import { Container, Row, Col, Nav, Link } from "react-bootstrap";
 
 const MenuBar = () => {
   return (
     <Container>
-        <Row>
-         <Nav.Link eventKey="link-event-key" className="">
-          Link text
-         </Nav.Link>
-        </Row>
+      
+                
     </Container>
   );
 };
